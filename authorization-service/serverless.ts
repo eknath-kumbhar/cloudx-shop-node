0751b412a48c78ac43d81b44efb2403cb625b8e1
eknath-kumbhar <eknathkumbharv1@hotmail.com>
2022-11-13 01:21:04 +0530

    task 7.1 changes

---
+++ authorization-service/serverless.ts
@@ -0,0 +1,55 @@
+import type { AWS } from '@serverless/typescript';
+
+import * as functions from '@functions/index';
+
+const serverlessConfiguration: AWS = {
+  service: 'authorization-service',
+  frameworkVersion: '3',
+  plugins: ['serverless-dotenv-plugin', 'serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    region: 'ap-south-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: ["execute-api:Invoke"],
+        Resource: ["*"]
+      }
+    ]
+  },
+  // import the function via paths
+  functions,
+  resources: {
+    Outputs: {
+      BasicAuthorizerArn: {
+        Value: {
+          "Fn::GetAtt": ["BasicAuthorizerLambdaFunction", "Arn"],
+        }
+      }
+    }
+  },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
